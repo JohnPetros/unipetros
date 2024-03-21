9c887886dd9d04312276e6760ad912b49d0c9539
johnpetros <joaopedro.nc@outlook.com>
2024-03-20 23:09:07 -0300

    ✨ feat: add get_about_page view

--- src/app/views/home_views.py
+++ src/app/views/home_views.py
@@ -11,3 +11,10 @@ def get_landing_page() -> str:
     return render_template(
         "/pages/home/landing/index.html", advantages=ADVANTAGES, posts=POSTS
     )
+
+
+@home_views.route("/about")
+def get_about_page() -> str:
+    return render_template(
+        "/pages/home/about/index.html", advantages=ADVANTAGES, posts=POSTS
+    )
