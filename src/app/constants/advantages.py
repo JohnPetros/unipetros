2d9f1416a7d8d3f38132e874f30e6a4f677267ff
johnpetros <joaopedro.nc@outlook.com>
2024-03-20 23:09:48 -0300

    ✨ feat (constants); add advantages

---
+++ src/app/constants/advantages.py
@@ -0,0 +1,32 @@
+ADVANTAGES = [
+    {
+        "title": "Suporte 24 horas",
+        "description": "Conte com o apoio de uma equipe especializada para tirar suas dúvidas e te auxiliar em seus estudos, a qualquer hora do dia ou da noite.",
+        "icon": "alarm",
+    },
+    {
+        "title": "Aproveitamento de estudos",
+        "description": "Dê valor ao seu tempo e conhecimento prévio. Transfira créditos de outras instituições e avance em sua graduação com mais rapidez.",
+        "icon": "books",
+    },
+    {
+        "title": "Comunidade de alunos",
+        "description": "Faça parte de uma comunidade vibrante e conectada. Conecte-se com outros estudantes, participe de eventos e construa sua rede de contatos.",
+        "icon": "student",
+    },
+    {
+        "title": "Desafios práticos",
+        "description": "Aprimore suas habilidades e desenvolva soluções inovadoras através de projetos reais e simulações que refletem o dia a dia do mercado.",
+        "icon": "atom",
+    },
+    {
+        "title": "Linhamento ao mercado",
+        "description": "Tenha acesso a uma grade curricular constantemente atualizada, elaborada por profissionais experientes e em sintonia com as demandas do mercado de trabalho.",
+        "icon": "briefcase",
+    },
+    {
+        "title": "Projetos de pesquisa",
+        "description": "Participe de projetos de pesquisa de ponta, contribuindo para o avanço da tecnologia e expandindo seus horizontes.",
+        "icon": "test-tube",
+    },
+]
