1997859ee2a0a02ad3bb25032a1280d814fa3064
johnpetros <joaopedro.nc@outlook.com>
2024-03-23 12:58:32 -0300

    🖥️ views: add @login_checker decorator to get_analytics_page view

--- src/app/views/admin_views.py
+++ src/app/views/admin_views.py
@@ -1,10 +1,11 @@
 from flask import Blueprint, render_template
-from auth import get_auth_user
+from auth import get_auth_user, login_checker
 
 admin_views = Blueprint("admin_views", __name__)
 
 
 @admin_views.route("/analytics")
+@login_checker
 def get_analytics_page() -> str:
     user = get_auth_user()
     return render_template("pages/admin/analytics/index.html", user=user)
