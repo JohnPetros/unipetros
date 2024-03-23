9ce6ac51e42cb4782df2105b52406c3d508a480c
johnpetros <joaopedro.nc@outlook.com>
2024-03-23 10:58:22 -0300

    🖥️ views: init auth_views and admin_views views

--- src/app/views/__init__.py
+++ src/app/views/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
+
 from .home_views import home_views
+from .auth_views import auth_views
+from .admin_views import admin_views
 
 
 def init_views(app: Flask) -> None:
     app.register_blueprint(home_views)
+    app.register_blueprint(auth_views)
+    app.register_blueprint(admin_views)
