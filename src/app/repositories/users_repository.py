32b78fd1162d1719bee358ab755b6c21648bc67d
johnpetros <joaopedro.nc@outlook.com>
2024-03-23 10:56:21 -0300

    🗄️ repositories: add user repository

---
+++ src/app/repositories/users_repository.py
@@ -0,0 +1,31 @@
+from typing import Union
+from models.user_model import UserModel
+from database import mysql
+
+
+class UsersRepository:
+    def get_user_by_id(self, id: str) -> Union[UserModel, None]:
+        print(id)
+        user = mysql.query(sql="SELECT * FROM users WHERE id= %s", params=[str(id)])
+
+        if not user:
+            return None
+
+        return UserModel(user)
+
+    def get_user_by_email(self, email: str) -> Union[UserModel, None]:
+        user = mysql.query(sql="SELECT * FROM users WHERE email= %s", params=[email])
+
+        if not user:
+            return None
+
+        return UserModel(user)
+
+    def save(self, user: UserModel) -> None:
+        mysql.mutate(
+            sql="""
+            INSERT INTO users (id, email, name, password, role) 
+            VALUES (%s, %s, %s, %s, %s)
+            """,
+            params=[user.id, user.email, user.name, user.password, user.role],
+        )
