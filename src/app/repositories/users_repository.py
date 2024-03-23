041865f1b523c0a3a5d26629f951c2fa46e42bbf
johnpetros <joaopedro.nc@outlook.com>
2024-03-23 12:59:00 -0300

    🐛 fix: ensure create user model insatance with id

--- src/app/repositories/users_repository.py
+++ src/app/repositories/users_repository.py
@@ -5,13 +5,14 @@ from database import mysql
 
 class UsersRepository:
     def get_user_by_id(self, id: str) -> Union[UserModel, None]:
+        user = mysql.query(sql="SELECT * FROM users WHERE id= %s", params=[id])
+
         print(id)
-        user = mysql.query(sql="SELECT * FROM users WHERE id= %s", params=[str(id)])
 
         if not user:
             return None
 
-        return UserModel(user)
+        return UserModel(user, user["id"])
 
     def get_user_by_email(self, email: str) -> Union[UserModel, None]:
         user = mysql.query(sql="SELECT * FROM users WHERE email= %s", params=[email])
@@ -19,7 +20,7 @@ class UsersRepository:
         if not user:
             return None
 
-        return UserModel(user)
+        return UserModel(user, user["id"])
 
     def save(self, user: UserModel) -> None:
         mysql.mutate(
