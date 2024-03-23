f05250c67ff4d37a1a939fffbbc689872fc46f00
johnpetros <joaopedro.nc@outlook.com>
2024-03-23 12:57:44 -0300

    📒 forms: remove DataRequired validator from LoginForm

--- src/app/forms/login_form.py
+++ src/app/forms/login_form.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
 
 
 class LoginForm(FlaskForm):
     email = StringField("E-mail", validators=[DataRequired(), Email()])
     password = PasswordField("Senha", validators=[Length(6, 20)])
-    submit_button = SubmitField("Login")
+    remember_me = BooleanField("Lembre-se de mim")
+    submit_button = SubmitField("Login", validators=[DataRequired()])
