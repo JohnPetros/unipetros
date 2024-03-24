c45befb2035e77c170f81e259a73032f7dc584e6
johnpetros <joaopedro.nc@outlook.com>
2024-03-24 10:39:41 -0300

    👤 models: add user model

--- src/app/models/user_model.py
+++ src/app/models/user_model.py
@@ -1,12 +1,11 @@
-from typing import Dict
-from uuid import uuid4 as generate_id
+from dataclasses import dataclass
+
+from models.model import Model
 
 
-class UserModel:
-    def __init__(self, user: Dict, id: str = None) -> None:
-        self.id = str(generate_id()) if id is None else id
-        self.name = user["name"]
-        self.email = user["email"]
-        self.password = user["password"]
-        self.avatar = user["avatar"]
-        self.role = user["role"]
+@dataclass
+class UserModel(Model):
+    name: str = None
+    email: str = None
+    password: str = None
+    avatar: str = None
