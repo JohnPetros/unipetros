1d00334f749a259c4c5d31f5afc1a54ba7f96e12
johnpetros <joaopedro.nc@outlook.com>
2024-03-24 10:39:32 -0300

    👤 models: add base model

---
+++ src/app/models/model.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+from uuid import uuid4 as generate_id
+
+
+@dataclass
+class Model:
+    id: str = None
+
+    def __post_init__(self):
+        self.id = str(generate_id()) if self.id is None else self.id
