5843e51da8b5f8e40f8f6ff12b0f38b8cfc9ad39
johnpetros <joaopedro.nc@outlook.com>
2024-03-25 19:26:03 -0300

    📒 forms: add professor form

---
+++ src/app/forms/professor_form.py
@@ -0,0 +1,10 @@
+from wtforms.validators import DataRequired
+
+from wtforms import SubmitField
+from .user_form import UserForm
+from .multi_checkbox_field import MultiCheckboxField
+
+
+class ProfessorForm(UserForm):
+    subjects = MultiCheckboxField("Disciplinas")
+    submit_button = SubmitField("Adicionar professor", validators=[DataRequired()])
