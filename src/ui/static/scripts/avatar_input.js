d3134592eebe555f72f3438113fa0ef600d0375b
johnpetros <joaopedro.nc@outlook.com>
2024-03-25 19:34:12 -0300

    💡 scripts: add avatar input script

---
+++ src/ui/static/scripts/avatar_input.js
@@ -0,0 +1,18 @@
+const avatar = document.querySelector("[data-avatar-input='avatar']")
+const input = document.querySelector("[data-avatar-input='input']")
+
+function handleInputChange(event) {
+  const file = event.target.files[0]
+  const fileReader = new FileReader()
+
+  if (file) {
+    fileReader.onload = (fileReaderEvent) => {
+      avatar.src = fileReaderEvent.target.result
+    }
+
+    fileReader.readAsDataURL(file)
+  }
+}
+
+if (input)
+  input.addEventListener('change', handleInputChange)
