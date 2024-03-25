bc2472c57ec9a85c0be1b4a389f77224cf75eb5d
johnpetros <joaopedro.nc@outlook.com>
2024-03-25 19:25:32 -0300

    🗃️ file: renmae app error to error

--- src/app/utils/app_error.py
+++
@@ -1,9 +0,0 @@
-from flask import abort
-from cowsay import func as cow_say
-
-
-class AppError(Exception):
-    def __init__(self, message: str, status_code: int = 500, should_abort=True) -> None:
-        cow_say(message)
-        if should_abort:
-            abort(message, status_code)
--- src/app/utils/error.py
+++ src/app/utils/error.py
@@ -6,4 +6,4 @@ class Error(Exception):
     def __init__(self, message: str, status_code: int = 500, should_abort=True) -> None:
         cow_say(message)
         if should_abort:
-            abort(message, status_code)
+            abort(status_code, message)
