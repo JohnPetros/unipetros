3cdad03ae877a537afd1c686916942cabef0fef5
johnpetros <joaopedro.nc@outlook.com>
2024-03-25 19:39:04 -0300

    🖥️ views: add error handlers

--- src/app/views/__init__.py
+++ src/app/views/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, flash, redirect
 
 from .home_views import home_views
 from .auth_views import auth_views
@@ -6,6 +6,15 @@ from .admin_views import admin_views
 
 
 def init_views(app: Flask) -> None:
-    app.register_blueprint(home_views)
     app.register_blueprint(auth_views)
+    app.register_blueprint(home_views)
     app.register_blueprint(admin_views)
+
+    @app.errorhandler(404)
+    def handle_not_found_error(_):
+        return "EITA"
+
+    @app.errorhandler(400)
+    def handle_bad_request_error(error):
+        flash(error.description, "error")
+        return redirect(request.path)
