a7c80a1f1f080f75420367c5876433b7ccb16272
johnpetros <joaopedro.nc@outlook.com>
2024-03-25 19:33:39 -0300

    🧰 utils: read file text

--- src/app/utils/file.py
+++ src/app/utils/file.py
@@ -1,15 +1,15 @@
 from pathlib import Path
-from utils.app_error import AppError
+from utils.error import Error
 
 
 class File:
     def __init__(self, folder, filename) -> None:
         self.path = Path(f"{folder}/{filename}")
 
-    def read(self) -> str:
+    def read_text(self) -> str:
         try:
             return self.path.read_text(encoding="utf-8")
         except Exception as exception:
-            raise AppError(
+            raise Error(
                 f"Failed to read {self.path.absolute()} file. Error: {exception}"
             ) from exception
