6b02353ca916eecc2d0ad32fd742fc3e52f105fa
johnpetros <joaopedro.nc@outlook.com>
2024-03-28 15:53:03 -0300

    👤 entities: add subjectsattribute to Course entity

--- src/app/entities/course.py
+++ src/app/entities/course.py
@@ -1,9 +1,13 @@
+from typing import List
+
 from dataclasses import dataclass
 
 from entities.entity import Entity
+from entities.subject import Subject
 
 
 @dataclass
 class Course(Entity):
     name: str = None
     description: str = None
+    subjects: List[Subject] = None
