45b0400f0096d8037e3f1fb6b708118fd25b07f7
johnpetros <joaopedro.nc@outlook.com>
2024-03-28 15:53:30 -0300

    💡 scripts: add students insertion sql script

--- src/app/database/scripts/students.sql
+++ src/app/database/scripts/students.sql
@@ -5,6 +5,7 @@ CREATE TABLE students (
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL UNIQUE,
   password VARCHAR(255) NOT NULL, 
+  phone CHAR(11) NOT NULL, 
   avatar VARCHAR(255) DEFAULT 'default-avatar.png',
   birthdate DATE NOT NULL,
   gender ENUM('male', 'female'),
@@ -12,11 +13,11 @@ CREATE TABLE students (
   FOREIGN KEY (course_id) REFERENCES courses(id) ON DELETE CASCADE
 );
 
-INSERT INTO students (name, email, password, birthdate, gender, course_id)
+INSERT INTO students (name, email, password, phone, birthdate, gender, course_id)
 VALUES
-('Lúcia Romena', 'lucia300@unipetros.com', '$2b$12$WrntejsV/WPVXRfM0EFPy.X6nvy1UCwNTgPDCmayvYfhsVANRxGo.', '2002-03-16', 'female', '01cbf711-ec80-11ee-8ced-0242ac130002'),
-('Milena Oliveira', 'milena7777@unipetros.com', '$2b$12$WrntejsV/WPVXRfM0EFPy.X6nvy1UCwNTgPDCmayvYfhsVANRxGo.', '2005-08-15', 'female', '01cd80a7-ec80-11ee-8ced-0242ac130002'),
-('Raquel Oliveira', 'milena8888@unipetros.com', '$2b$12$WrntejsV/WPVXRfM0EFPy.X6nvy1UCwNTgPDCmayvYfhsVANRxGo.', '2006-11-12', 'female', '01cdc9fe-ec80-11ee-8ced-0242ac130002');
+('Lúcia Romena', 'lucia300@unipetros.com', '$2b$12$WrntejsV/WPVXRfM0EFPy.X6nvy1UCwNTgPDCmayvYfhsVANRxGo.', '12894561235', '2002-03-16', 'female', '01cbf711-ec80-11ee-8ced-0242ac130002'),
+('Milena Oliveira', 'milena7777@unipetros.com', '$2b$12$WrntejsV/WPVXRfM0EFPy.X6nvy1UCwNTgPDCmayvYfhsVANRxGo.', '21456789632', '2005-08-15', 'female', '01cd80a7-ec80-11ee-8ced-0242ac130002'),
+('Raquel Oliveira', 'milena8888@unipetros.com', '$2b$12$WrntejsV/WPVXRfM0EFPy.X6nvy1UCwNTgPDCmayvYfhsVANRxGo.', '22456789635', '2006-11-12', 'female', '01cdc9fe-ec80-11ee-8ced-0242ac130002');
 
 DELETE FROM students;
 
