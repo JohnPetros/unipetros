c3b4698ebfe05b0877e15f737ccd9c332810fe65
johnpetros <joaopedro.nc@outlook.com>
2024-03-29 21:17:00 -0300

    🧰 utils: send a error message to ui in Error class

--- src/app/utils/error.py
+++ src/app/utils/error.py
@@ -1,9 +1,18 @@
-from flask import abort
+from flask import abort, flash
 from cowsay import func as cow_say
 
 
 class Error(Exception):
-    def __init__(self, message: str, status_code: int = 500, should_abort=True) -> None:
-        cow_say(message)
+    def __init__(
+        self,
+        error_message: str,
+        status_code: int = 500,
+        ui_message="",
+        should_abort=True,
+    ) -> None:
+        cow_say(error_message)
+        if ui_message:
+            flash(ui_message, "error")
+
         if should_abort:
-            abort(status_code, message)
+            abort(status_code, error_message)
