066631637712050b517aa181599c9cda175e8db8
johnpetros <joaopedro.nc@outlook.com>
2024-03-30 12:47:11 -0300

    ✨ feat: get students absents count by range days

--- src/app/use_cases/admin_use_cases/get_analytics_page_data_use_case.py
+++ src/app/use_cases/admin_use_cases/get_analytics_page_data_use_case.py
@@ -1,3 +1,6 @@
+from typing import Dict
+from datetime import date, timedelta
+
 from repositories import (
     professors_repository,
     students_repository,
@@ -22,6 +25,20 @@ class GetAnalyticsPageDataUseCase:
 
             last_enrolled_students = students_repository.get_last_enrolled_students()
 
+            students_absents = students_repository.get_students_absents()
+
+            students_absents_count_by_range_days = {
+                "7 days": self.__get_students_absents_count_by_range_days(
+                    7, students_absents, total_students
+                ),
+                "30 days": self.__get_students_absents_count_by_range_days(
+                    30, students_absents, total_students
+                ),
+                "90 days": self.__get_students_absents_count_by_range_days(
+                    90, students_absents, total_students
+                ),
+            }
+
             total_posts_by_category = posts_repository.get_posts_count_by_category()
 
             popular_courses = courses_repository.get_courses_ordered_by_students_count()
@@ -34,6 +51,31 @@ class GetAnalyticsPageDataUseCase:
                 "total_posts_by_category": total_posts_by_category,
                 "last_enrolled_students": last_enrolled_students,
                 "popular_courses": popular_courses,
+                "students_absents_count_by_range_days": students_absents_count_by_range_days,
             }
         except Exception as exception:
             raise Error(error_message=exception) from exception
+
+    def __get_students_absents_count_by_range_days(
+        self, range_days: int, students_absents: Dict, total_students_count: int
+    ) -> Dict:
+        current_date = date.today()
+
+        absents_count_by_date = []
+
+        for days in range(range_days, -1, -1):
+            first_date = current_date - timedelta(days=days)
+
+            count = 0
+            for absent in students_absents:
+                if absent["date"] == first_date:
+                    count += 1
+
+            absents_count_by_date.append(
+                {
+                    "date": first_date.strftime("%d/%m/%Y"),
+                    "absents_count": total_students_count - count,
+                }
+            )
+
+        return absents_count_by_date
