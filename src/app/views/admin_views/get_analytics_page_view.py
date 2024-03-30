9c4067921962c8cc02411ea5d2a4e42daea9cbab
johnpetros <joaopedro.nc@outlook.com>
2024-03-29 21:23:29 -0300

    🖥️ views: render charts on analytics page

--- src/app/views/admin_views/get_analytics_page_view.py
+++ src/app/views/admin_views/get_analytics_page_view.py
@@ -2,9 +2,24 @@ from flask import render_template
 
 from auth import get_auth_user, login_checker, role_checker
 
+from use_cases.admin_use_cases import get_analytics_page_data_use_case
+
 
 @login_checker
 @role_checker("admin")
 def get_analytics_page_view() -> str:
     user = get_auth_user()
-    return render_template("pages/admin/analytics/index.html", user=user)
+
+    data = get_analytics_page_data_use_case.execute()
+
+    return render_template(
+        "pages/admin/analytics/index.html",
+        user=user,
+        total_professors=data["total_professors"],
+        total_students=data["total_students"],
+        total_subjects=data["total_subjects"],
+        popular_courses=data["popular_courses"],
+        total_students_by_gender=data["total_students_by_gender"],
+        total_posts_by_category=data["total_posts_by_category"],
+        last_enrolled_students=data["last_enrolled_students"],
+    )
