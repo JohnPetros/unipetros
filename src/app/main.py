201a2bf4ff6b4b74fd96080bcab39a2d3903cfe7
johnpetros <joaopedro.nc@outlook.com>
2024-03-30 10:02:15 -0300

    🧪 test: husky pre commit

--- src/app/main.py
+++ src/app/main.py
@@ -6,10 +6,12 @@ from views import init_views
 from auth import init_auth
 
 
-def create_app() -> Flask:
+def init_app() -> Flask:
     app = Flask(
         __name__, template_folder="../ui/templates", static_folder="../ui/static"
     )
+
+    eitaEITA = "petros"
 
     app.config["SECRET_KEY"] = getenv("SECRET_KEY")
 
