5115d29945af9643db28af0d7b318dea4c79b974
johnpetros <joaopedro.nc@outlook.com>
2024-04-07 14:12:29 -0300

    🎨 lint: organize admin views by enitity

--- src/app/infra/views/admin_views/__init__.py
+++ src/app/infra/views/admin_views/__init__.py
@@ -1,13 +1,18 @@
 from flask import Blueprint
 
 from .get_analytics_page_view import get_analytics_page_view
+
 from .get_professor_details_page_view import get_professor_details_page_view
-from .handle_professors_page_view import handle_professors_page_view
-from .handle_subjects_page_view import handle_subjects_page_view
-from .handle_students_page_view import handle_students_page_view
+from .get_professors_page_view import get_professors_page_view
+from .get_filtered_professors_view import get_filtered_professors_view
 from .create_professors_by_csv_view import create_professors_by_csv_view
-from .create_subjects_by_csv_view import create_subjects_by_csv_view
+
+from .handle_students_page_view import handle_students_page_view
 from .create_students_by_csv_view import create_students_by_csv_view
+
+from .handle_subjects_page_view import handle_subjects_page_view
+from .create_subjects_by_csv_view import create_subjects_by_csv_view
+
 from .handle_courses_page_view import handle_courses_page_view
 from .create_courses_by_csv_view import create_courses_by_csv_view
 
@@ -19,8 +24,14 @@ route(rule="/dashboard/analytics", view_func=get_analytics_page_view)
 
 route(
     rule="/dashboard/professors",
-    view_func=handle_professors_page_view,
-    methods=["GET", "POST"],
+    view_func=get_professors_page_view,
+    methods=["GET"],
+)
+
+route(
+    rule="/dashboard/professors/filter",
+    view_func=get_filtered_professors_view,
+    methods=["GET"],
 )
 
 route(
