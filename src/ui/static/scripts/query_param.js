39f19c33827b304c1bd5aaee87066e718444e3cc
JohnPetros <joaopedro.nc@outlook.com>
2024-04-28 19:24:31 -0300

    🧱 templates: add checkbox to porfessors table

--- src/ui/static/scripts/query_param.js
+++ src/ui/static/scripts/query_param.js
@@ -1,13 +1,24 @@
 class QueryParam {
+  constructor() {
+    this.updateHtmxUrl()
+  }
+
   get(key) {
     const params = new URLSearchParams(document.location.search)
     return params.get(key)
+  }
+
+  getAll() {
+    const params = new URLSearchParams(document.location.search)
+    return Array.from(params)
   }
 
   append(key, value) {
     const url = new URL(location)
     url.searchParams.set(key, value)
     history.pushState({}, "", url)
+
+    this.updateHtmxUrl()
   }
 
   remove(key) {
@@ -15,5 +26,26 @@ class QueryParam {
     url.searchParams.delete(key)
     const newUrl = url.search ? url.href : url.href.replace("?", "")
     window.history.replaceState({}, document.title, newUrl)
+
+    this.updateHtmxUrl()
+  }
+
+  updateHtmxUrl() {
+    const htmxElements = document.querySelectorAll('[data-query-param^="hx-"]')
+
+    if (!htmxElements.length) return
+
+    for (const htmxElement of htmxElements) {
+      const htmxMethod = htmxElement.dataset.queryParam
+      const htmxUrl = htmxElement.getAttribute(htmxMethod)
+
+      const allParams = this.getAll()
+
+      const params = allParams
+        .map((param) => `${param[0]}=${param[1]}`)
+        .join("&")
+
+      htmxElement.setAttribute(htmxMethod, `${htmxUrl}?${params}`)
+    }
   }
 }
