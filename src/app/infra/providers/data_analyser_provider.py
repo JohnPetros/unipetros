4a3b143888d46925f47ce6a38526d0d7f9086c45
JohnPetros <joaopedro.nc@outlook.com>
2024-05-19 13:46:42 -0300

    ➕ providers: convert dataframe to excel

--- src/app/infra/providers/data_analyser_provider.py
+++ src/app/infra/providers/data_analyser_provider.py
@@ -1,5 +1,7 @@
 from typing import Dict, List
 
+from werkzeug.datastructures import FileStorage
+
 from pandas import DataFrame, read_excel, read_csv
 
 
@@ -10,15 +12,17 @@ class DataAnalyserProvider:
         self.data = None
 
     def analyse(self, data) -> None:
-        self.data = data
+        self.data = DataFrame(data)
 
-    def read_excel(self) -> None:
-        if self.__has_dataframe():
-            self.data = read_excel(self.data)
+    def read_excel(self, file: FileStorage) -> None:
+        self.data = read_excel(file)
 
-    def read_csv(self) -> None:
+    def read_csv(self, file: FileStorage) -> None:
+        self.data = read_csv(file)
+
+    def convert_to_excel(self, folder: str, filename: str) -> None:
         if self.__has_dataframe():
-            self.data = read_csv(self.data)
+            self.data.to_excel(f"{folder}/{filename}", index=False)
 
     def convert_to_list_of_records(self) -> List[Dict] | None:
         if self.__has_dataframe():
@@ -26,17 +30,8 @@ class DataAnalyserProvider:
 
         return None
 
+    def get_data(self):
+        return self.data
+
     def __has_dataframe(self) -> bool:
         return self.data is not None
-
-
-#  {'Nome': ['Rodrigo Branas', 'Ana Maria', 'Carlos Eduardo', 'Maria Fernanda', 'João Silva', 'Mariana Santos', 'Pedro Almeida', 'Juliana Oliveira'], 'E-mail': ['rodrigo@unipetros.com', 'anamaria@example.com', 'carlos@email.com', 'maria@email.com', 'joao@email.com', 'mariana@email.com', 'pedro@email.com', 'juliana@email.com'], 'Telefone': [12988815499, 987654321, 987654321, 987654321, 987654321, 987654321, 987654321, 987654321], 'Data de nascimento': [Timestamp('1990-12-12 00:00:00'), Timestamp('1985-06-05 00:00:00'), Timestamp('1992-06-15 00:00:00'), Timestamp('1993-07-20 00:00:00'), Timestamp('1995-10-10 00:00:00'), Timestamp('1998-03-25 00:00:00'), Timestamp('1990-09-03 00:00:00'), Timestamp('1991-11-18 00:00:00')], 'Gender': ['masculino', 'feminino', 'masculino', 'feminino', 'masculino', 'feminino', 'masculino', 'feminino'], 'Disciplinas': ['biologia,matematica', 'português,história', 'física,química', 'literatura,arte', 'geografia,inglês', 'química,biologia', 'matemática,física', 'inglês,história']}
-
-#  [
-#    {
-#      'Nome': 'Rodrigo Branas',
-#      'E-mail': 'rodrigo@unipetros.com',
-#      'Telefone': 12988815499,
-#      'Data de nascimento': Timestamp('1992-06-15 00:00:00')
-#    }
-#  ]
