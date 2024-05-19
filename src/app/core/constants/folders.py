7fd7febd6d8fc8f3fff82e3472f10567db35ba78
JohnPetros <joaopedro.nc@outlook.com>
2024-05-19 13:46:14 -0300

    🐛 fix: tmp folder path

--- src/app/core/constants/folders.py
+++ src/app/core/constants/folders.py
@@ -3,6 +3,6 @@ from pathlib import Path
 path = Path()
 
 FOLDERS = {
-    "tmp": path.absolute() / "src" / "app" / "tmp",
+    "tmp": path.absolute() / "src" / "app" / "infra" / "tmp",
     "uploaded_images": path.absolute() / "src" / "ui" / "static" / "uploads" / "images",
 }
